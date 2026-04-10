a8905999d35848a42eeaf94069897bc4b44a83ff
shift <drugzarbest@gmail.com>
2026-04-10 15:03:56 +0200

    Change rendering logic to eventually support header and trailer.

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,18 @@ use rust_simple_tui::*;
 
 // todo add pub function to setup raw mode and enter alt screen so to not add crossterm everywhere
 // todo add headingsymbol and trailing symbol
-// todo selection custoisation
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
     stdout().execute(EnterAlternateScreen)?;
 
     let mut foot: Menu = Menu::new();
-    foot.add_label("demomenu".to_string());
-    foot.add_action("An action".to_string(), "six".to_string());
-    foot.add_label("A line".to_string());
-    foot.add_label("A line".to_string());
-    foot.add_action("Doggoprit".to_string(), "doggoprint ".to_string());
-    foot.add_label("They call me the labler".to_string());
+    foot.add_label("demomenu".to_string(), "".to_string(), "".to_string());
+    foot.add_action("An action".to_string(), "six".to_string(), "".to_string(), "".to_string());
+    foot.add_label("A line".to_string(), "".to_string(), "".to_string());
+    foot.add_label("A line".to_string(), "".to_string(), "".to_string());
+    foot.add_action("Doggoprintttttttttttttttttttttttt".to_string(), "doggoprint ".to_string(), "".to_string(), "".to_string());
+    foot.add_label("They call me the labler".to_string(), "".to_string(), "".to_string());
 
     let res=foot.render()?;
     stdout().execute(LeaveAlternateScreen)?;
--- src/simpletui/ui.rs
+++ src/simpletui/ui.rs
@@ -1,16 +1,18 @@
 use std::io;
 use std::io::Write;
+use std::string::ToString;
 use crossterm::{event};
 use crossterm::event::{Event};
+use crossterm::terminal;
 
 enum MenuEntry {
-    Label(String),
-    Action{ label: String, id: String },
+    Label{label: String, header: String, footer: String},
+    Action{ label: String, id: String, header: String, footer: String },
 }
 
 pub struct Menu {
     entries: Vec<MenuEntry>,
-    cursor : usize
+    cursor : usize,
 }
 
 impl Menu {
@@ -21,12 +23,12 @@ impl Menu {
         }
     }
 
-    pub fn add_label(&mut self, label: String) {
-        self.entries.push(MenuEntry::Label(label));
+    pub fn add_label(&mut self, label: String, header: String, footer: String) {
+        self.entries.push(MenuEntry::Label{label, header, footer});
     }
 
-    pub fn add_action(&mut self, label: String, id: String) {
-        self.entries.push(MenuEntry::Action{label, id});
+    pub fn add_action(&mut self, label: String, id: String, header: String, footer: String) {
+        self.entries.push(MenuEntry::Action{label, id, header, footer});
     }
 
     fn select_at_index(&self, index: usize) -> Option<&String> {
@@ -53,12 +55,29 @@ impl Menu {
         self.cursor = new_cursor as usize
     }
 
+    fn prepare_entry(string: &str, max_width: usize, left: &str, right: &str, repeat_border: bool) -> Vec<String> {
+        let allowed_strlen = max_width - left.len() - right.len();
+        // TODO take up less ram
+        let chars : Vec<char> = string.chars().collect();
+        let mut output: Vec<String> = Vec::new();
+
+        for (i, chunk) in chars.chunks(allowed_strlen).enumerate() {
+            let line_text: String = chunk.iter().collect();
+            if (i == 0 || repeat_border) {
+                output.push(format!("{}{}{}", left, line_text, right));
+                continue
+            }
+            output.push(format!("{:width$}{}", "", line_text, width = left.len()));
+        }
+        output
+    }
+
     fn prepare_render(&mut self) {
         while self.cursor < self.entries.len() && !self.is_selectable(self.cursor) {
             self.cursor += 1;
         }
         if self.cursor >= self.entries.len() {
-            self.add_action("No actions provided, idiot.".to_string(), "".to_string());
+            self.add_action("No actions provided, idiot.".to_string(), "".to_string(), "".to_string(), "".to_string());
             self.cursor = 0;
         }
     }
@@ -68,17 +87,26 @@ impl Menu {
         crossterm::queue!(out, crossterm::terminal::Clear(crossterm::terminal::ClearType::All))?;
         crossterm::queue!(out, crossterm::cursor::MoveTo(0, 0))?;
 
+        let (width, _) = terminal::size().unwrap_or((80, 24));
+        let mut newline_index = 0;
         for (i, entry) in self.entries.iter().enumerate() {
-            crossterm::queue!(out, crossterm::cursor::MoveTo(0, i as u16))?;
+            let mut print_string : String = String::new();
+            let mut header : String = String::new();
+            let mut footer : String = String::new();
             match entry {
-                MenuEntry::Label(label) => write!(out ,"{}", label)?,
+                MenuEntry::Label{label, ..} => print_string = label.to_string(),
                 MenuEntry::Action{label, ..} => {
                     if i == self.cursor {
-                        write!(out,"> {}", label)?;
+                        print_string = format!("> {}", label.to_string());
                     } else {
-                        write!(out,"  {}", label)?;
+                        print_string = format!("  {}", label.to_string());
                     }
                 }
+            }
+            for line in Self::prepare_entry(&print_string, 24, "| ", "", true) {
+                write!(out, "{}", line)?;
+                newline_index += 1;
+                crossterm::queue!(out, crossterm::cursor::MoveTo(0, newline_index as u16))?
             }
         }
         out.flush()
