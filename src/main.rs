1198f1ac7a484c202ea1f2305104b04a0a0fca69
shift <drugzarbest@gmail.com>
2026-04-27 09:44:29 +0200

    Add the ability to configure the border repeating

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,19 @@ use std::io;
 use simpletui::ui::Menu;
 use rust_simple_tui::*;
 use rust_simple_tui::simpletui::ui::{enter_tui, leave_tui};
-// TODO: Toggle for repeat border
 
 fn main() -> io::Result<()> {
 
     let mut foot: Menu = Menu::default();
-    foot.bordered_label("--------------", Some("┌-"), Some("-┐"));
-    foot.bordered_action( "[X]", "" ,Some("| DEMOMENU "), Some(" |"));
-    foot.bordered_label("--------------", Some("├-"), Some("-┤"));
-    foot.bordered_action("An action", "six", Some("| "), Some(" |"));
-    foot.bordered_label("A line", Some("| "), Some(" |"));
-    foot.bordered_label("A line", Some("| "), Some(" |"));
-    foot.bordered_action("Doggoprinter", "doggoprint ", Some("| "), Some(" |"));
-    foot.bordered_label("the labler", Some("| "), Some(" |"));
-    foot.bordered_label("--------------", Some("└-"), Some("-┘"));
+    foot.bordered_label("--------------", Some("┌-"), Some("-┐"), false);
+    foot.bordered_action( "[X]", "" ,Some("| DEMOMENU "), Some(" |"), false);
+    foot.bordered_label("--------------", Some("├-"), Some("-┤"), false);
+    foot.bordered_action("An action", "six", Some("| "), Some(" |"), true);
+    foot.bordered_label("A line", Some("| "), Some(" |"), true);
+    foot.bordered_label("A line", Some("| "), Some(" |"), true);
+    foot.bordered_action("Doggoprinter", "doggoprint ", Some("| "), Some(" |"), true);
+    foot.bordered_label("the labler", Some("| "), Some(" |"), true);
+    foot.bordered_label("--------------", Some("└-"), Some("-┘"), false);
 
     enter_tui()?;
     let res=foot.render(3)?;
